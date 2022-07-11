c9fb3bd707fc6c7ca7d07aa84e1a3b973f0c54ce
Svetoslav <68899725+Mastera21@users.noreply.github.com>
2022-07-11 15:38:02 +0300

    Implementing Renderer class

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -1,1 +1,27 @@
 #include "Renderer.h"
+
+#include "Walnut/Random.h"
+
+void Renderer::OnResize(uint32_t width, uint32_t height) {
+
+	if (_finalImage) {
+		//No resize needed
+		if (_finalImage->GetWidth() == width && _finalImage->GetHeight() == height) {
+			return;
+		}
+		_finalImage->Resize(width, height);
+	}else {
+		_finalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+	}
+
+	delete[] _imageData;
+	_imageData = new uint32_t[width * height];
+}
+
+void Renderer::Render() {
+	for (uint32_t i = 0; i < _finalImage->GetWidth() * _finalImage->GetHeight(); i++) {
+		_imageData[i] = Walnut::Random::UInt();
+		_imageData[i] |= 0xff000000;
+	}
+	_finalImage->SetData(_imageData);
+}
--- RayTracing/src/Renderer.h
+++ RayTracing/src/Renderer.h
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <memory>
+
+#include "Walnut/Image.h"
+
 class Renderer{
 public:
+	Renderer() = default;
+
+	void OnResize(uint32_t width, uint32_t height);
+	void Render();
+
+	std::shared_ptr<Walnut::Image> GetFinalImage() const { return _finalImage; };
 
 private:
-
+	std::shared_ptr<Walnut::Image> _finalImage;
+	uint32_t* _imageData = nullptr;
 };
 
